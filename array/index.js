1e493fb342ebdd8d490c5046f9e813bf2d1026de
Amit <akumar00029@gmail.com>
2021-06-07 09:33:50 -0700

    array method updates

--- array/index.js
+++ array/index.js
@@ -3,6 +3,12 @@ function arrayOperation(){
     this.length = 0
 }
 
+// update item at index
+arrayOperation.prototype.setValue = function (pos, item){
+    this.array[pos] = item
+}
+
+
 // item pos start from 0
 
 arrayOperation.prototype.size = function () {
@@ -18,10 +24,11 @@ arrayOperation.prototype.insert =  function(pos,item){
     this.incrementLength()
 }
 
-// update item at index
-arrayOperation.prototype.setValue = function (pos, item){
-    this.array[pos] = item
+// insert item at index 0
+arrayOperation.prototype.unshift = function (item) {
+    this.insert(0,item)
 }
+
 
 //increment length
 arrayOperation.prototype.incrementLength = function (){
@@ -33,24 +40,42 @@ arrayOperation.prototype.decrementLength = function () {
     return --this.length;
 }
 
-// insert item at index 0
-arrayOperation.prototype.unshift = function (item) {
-    this.insert(0,item)
+arrayOperation.prototype.checkPropertyExists = function (index){
+    if (this.array.hasOwnProperty(index))
+        return true;
+    throw "Don't have property on array"
 }
+
+arrayOperation.prototype.valueAt = function (index){
+    if (this.checkPropertyExists(index))
+        return this.array[index];
+}
+
+//remove element at index
+arrayOperation.prototype.removeAt = function (index){
+    if (this.checkPropertyExists(index)){
+        for (let i = index; i< this.size(); i++){
+            this.setValue(index, this.valueAt(index + 1))
+        }
+        this.deleteAt(this.size() - 1)
+        this.decrementLength()
+    }
+}
+
 
 // delete item at index 0
 arrayOperation.prototype.shift = function (){
-
+    this.removeAt(0)
 }
-
 // insert item at last index
-arrayOperation.prototype.append = function (item){
 
+arrayOperation.prototype.append = function (item){
+    this.insert(this.array.size(),item)
 }
 
 // delete item at last index
 arrayOperation.prototype.pop = function (){
-
+    this.removeAt(this.array.size() - 1)
 }
 
 // return index of item
@@ -58,24 +83,11 @@ arrayOperation.prototype.indexOf = function (item){
 
 }
 
-arrayOperation.prototype.valueAt = function (index){
-    if (this.array.hasOwnProperty(index))
-        return this.array[index];
-    throw "Don't have property on array"
-}
-
-//remove element at index
-arrayOperation.prototype.removeAt = function (index){
-    for (let i = index; i< this.size(); i++){
-        this.setValue(index, this.valueAt(index + 1))
-    }
-    this.deleteAt(this.size() - 1)
-    this.decrementLength()
-}
-
+// for internal use
 arrayOperation.prototype.deleteAt = function (pos){
     delete this.array[pos];
 }
+
 
 arrayOperation.prototype.removeItem = function (item){
 
@@ -84,4 +96,9 @@ arrayOperation.prototype.removeItem = function (item){
 arr = new arrayOperation()
 arr.insert(0,20)
 arr.unshift(30)
-console.log(arr)
+console.log(arr.valueAt(0))
+try {
+    console.log(arr.valueAt(5))
+}catch (e){
+    console.log({e})
+}
--- main.js
+++ main.js
@@ -61,3 +61,13 @@ inquirer.prompt({
     })
 */
 
+use strict;
+
+const fs = require('fs')
+
+process.stdin.resume()
+process.stdin.setEncoding('utf-8')
+
+
+
+
