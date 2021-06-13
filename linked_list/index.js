b9dba671e83aaa6f834db79150009b3273e83beb
Amit <akumar00029@gmail.com>
2021-06-13 02:42:27 -0700

    linklist add element working

--- linked_list/index.js
+++ linked_list/index.js
@@ -30,6 +30,10 @@ linkList.prototype.incrementLength = function() {
     return this.length++
 }
 
+linkList.prototype.decrementLength = function() {
+    return this.length--
+}
+
 linkList.prototype.unshift = function(node){
 
     this.incrementLength()
@@ -45,12 +49,52 @@ linkList.prototype.unshift = function(node){
 }
 
 linkList.prototype.shift = function(){
+    this.decrementLength()
     this.node = this.node.next
     this.node.prev = null
 }
 
+linkList.prototype.lastNode = function () {
+    let currentNode = this.node
+    for (let index = 0; index < this.size() -2; index++) {
+        currentNode = currentNode.next
+    }
+    return currentNode;
+}
+
+linkList.prototype.push = function(node){
+    this.incrementLength()
+    let lastNode = this.lastNode()
+    lastNode.next = node
+    node.prev = lastNode
+}
+
 linkList.prototype.addNode = function(pos, node){
 
+    if (pos > this.size()) {
+        throw "index Out of Bound";
+    }
+
+    if (pos === 0) {
+        this.unshift(node);
+        return;
+    }
+
+    if (pos === this.size()) {
+        this.push(node)
+        return;
+    }
+
+    let currentNode = this.node
+    for (let index = 0; index < pos - 1; index++) {
+        currentNode = currentNode.next
+    }
+    this.incrementLength()
+    let nextNode = currentNode.next
+    currentNode.next = node
+    node.prev = currentNode
+    node.next = nextNode
+    nextNode.prev = node
 }
 
 let linklist = new linkList()
@@ -62,4 +106,6 @@ linklist.unshift(new Node(-1))
 linklist.unshift(new Node(-111))
 linklist.unshift(new Node(-11111))
 linklist.shift()
-console.log(linklist);
+
+linklist.addNode(3,new Node(3));
+console.dir(linklist, {depth: 4});
